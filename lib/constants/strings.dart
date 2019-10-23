38ae4208719dfc2cf879b6ddad3c25f3368618bc
NagashreeBhat <nagashreesbhat@gmail.com>
2019-10-23 14:54:04 -0700

    First look of my portfolio

---
+++ lib/constants/strings.dart
@@ -0,0 +1,20 @@
+class Strings {
+  Strings._();
+
+  static const String about_me = 'About Me';
+  static const String about = 'About';
+  static const String me = ' Me';
+  static const String portfoli = 'Portfoli';
+  static const String o = 'o';
+  static const String headline = 'I\'am Nagashree Bhat, Mobile App Developer from San Francisco, United States';
+  static const String summary = 'Focused professional having excellent technical and communication skills, and offering 4 years of experience in Computer industry. Proficient in Data Structures, Object Oriented Concepts, Cloud Technologies, Windows & Linux Server environments. Specialize in thinking outside the box to find unique solutions to difficult engineering problems. Passionate about Mobile App & SDKs, Cloud and Web technologies, UI and UX design.';
+  static const String experience = 'Experience';
+  static const String skills_i_have = 'My Current Skills';
+  static const String rights_reserved = '© 2019 NAGASHREE BHAT. ALL RIGHTS RESERVED.';
+
+  // menu items
+  static const String menu_home = 'Home';
+  static const String menu_about = 'Github';
+  static const String menu_contact = 'Resume';
+
+}
