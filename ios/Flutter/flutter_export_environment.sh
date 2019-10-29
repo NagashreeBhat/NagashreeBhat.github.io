b907fa8f68f9bfd0b8959a7c6bd2db7e127f8c66
NagashreeBhat <nagashreesbhat@gmail.com>
2019-10-28 20:58:29 -0700

    webdevu build

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/nagashreebhat/Documents/flutter 2"
+export "FLUTTER_APPLICATION_PATH=/Users/nagashreebhat/Documents/portfoliowithFlutter-master"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/nagashreebhat/Documents/flutter 2/bin/cache/artifacts/engine/ios"
